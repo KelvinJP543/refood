ced0c36d48ffbbfc89d5afc6a3b4894b6a4366a7
Kelvin Jaya Pratama <144788025+KelvinJP543@users.noreply.github.com>
2024-12-22 20:05:25 +0700

    Update config.js
    test disable

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
-  BASE_URL: 'https://refood-navy.vercel.app/api/',
-  BASE_IMAGE_URL: 'https://refood-navy.vercel.app/api/images/',
-  DEFAULT_LANGUAGE: 'en-us',
+  // BASE_URL: 'https://refood-navy.vercel.app/api/',
+  // BASE_IMAGE_URL: 'https://refood-navy.vercel.app/api/images/',
+  // DEFAULT_LANGUAGE: 'en-us',
 };
 
 export default CONFIG;
